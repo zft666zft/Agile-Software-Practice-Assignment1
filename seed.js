4f01eaa1f6dd5fef9b73633df25d6fb88f963b71
zft666zft <2810458566@qq.com>
2024-11-02 21:56:00 +0000

    make preparations for database seeding and install mongodb

---
+++ seed.js
@@ -0,0 +1,29 @@
+const MongoClient = require('mongodb').MongoClient;
+const fs = require('fs');
+
+// MongoDB URL from the Docker Compose environment variable
+const url = 'mongodb://admin:password@localhost:27017';
+const dbName = 'movies';
+const client = new MongoClient(url);
+
+async function seedDB() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+    const collection = db.collection('movies');
+
+    // Load movie data from seeding.json
+    const movies = JSON.parse(fs.readFileSync('seeding.json', 'utf8'));
+
+    // Insert movies into collection
+    const result = await collection.insertMany(movies);
+    console.log(`${result.insertedCount} movies inserted`);
+  } catch (err) {
+    console.error('Failed to seed database:', err);
+  } finally {
+    await client.close();
+  }
+}
+
+seedDB();
